2a4fea29ddedd7e954330b6496f78713a8490111
Jason R B Taylor <jasonrbtaylor@gmail.com>
2016-09-29 10:34:37 -0400

    add restart

--- features.py
+++ features.py
@@ -29,16 +29,22 @@ lasagne.layers.set_all_param_values(cnn['prob'],pretrained['param values'])
 def frame_feature(x):
     return numpy.array(lasagne.layers.get_output(cnn['fc7'],x,
                                               deterministic=True).eval())
+batch_size = 100 # limit video length to avoid out-of-memory errors
 dataloader = data.loader()
 nfiles = len(dataloader.filenames)
 features_path = os.path.join(os.curdir,os.path.join('framefeatures'))
 if not os.path.isdir(features_path):
     os.mkdir(features_path)
-for idx in range(nfiles):
+startAt = 0 # in case this crashes partway through
+for idx in range(startAt,nfiles):
     print(('idx %i / %i') % (idx,nfiles))
     video,label,group,clip = dataloader.get(idx)
     filename = 'l'+str(label)+'_g'+str(group)+'_c'+str(clip)+'.pkl'
-    pickle.dump(frame_feature(video),
+    features = numpy.zeros((video.shape[0],4096),dtype='float32')
+    for f in range(0,video.shape[0],batch_size):
+        batch_idx = range(f,numpy.min([f+batch_size,video.shape[0]]))
+        features[batch_idx] = frame_feature(video[batch_idx])
+    pickle.dump(features,
                 open(os.path.join(features_path,filename),'wb'))
 
 
